1e2536b56af0787bada500d208d9672d8359dd1d
Nadav Tasger <tashernadav@gmail.com>
2019-09-20 21:17:45 +0300

    Backend for EastShaing

---
+++ Challenges/EasyShaing/src/scripts/backend/manager/manage.php
@@ -0,0 +1,37 @@
+<?php
+
+const WORDS_FILE_PATH = __DIR__ . "/../../../files/passwords.txt";
+const DATABASE_FILE_PATH = __DIR__ . "/../../../files/database.json";
+
+$database = json_decode(file_get_contents(DATABASE_FILE_PATH));
+
+api("manage", function ($action, $callback) {
+    global $database;
+    if ($action === "session_welcome") {
+        $id = random(256);
+        $word = sha1(select_word());
+        $database->$id = $word;
+        file_put_contents(DATABASE_FILE_PATH, json_encode($database));
+        return [true, [$id, $word]];
+    } else if ($action === "check") {
+        if (isset($parameters->session) && isset($parameters->word)) {
+            if(sha1($parameters->word) === $database->{$parameters->session}){
+                unset($database->{$parameters->session});
+                file_put_contents(DATABASE_FILE_PATH, json_encode($database));
+                return [true, "NAT{t00_3asy_4or_me3_2789546347}"];
+            }
+        } else {
+            return [false, "Missing parameters"];
+        }
+    }
+    return [false, "Unknown action"];
+}, false);
+
+echo json_encode($result);
+
+function select_word()
+{
+    $words = explode("\n", file_get_contents(WORDS_FILE_PATH));
+    shuffle($words);
+    return $words[0];
+}
