fd12b1d878983193cfa1ee015cce2d4ff22bbbcc
Nadav Tasger <tashernadav@gmail.com>
2019-09-20 21:32:37 +0300

    Finished EasyShaing

--- Challenges/EasyShaing/src/scripts/backend/manager/manage.php
+++ Challenges/EasyShaing/src/scripts/backend/manager/manage.php
@@ -5,7 +5,7 @@ const DATABASE_FILE_PATH = __DIR__ . "/../../../files/database.json";
 
 $database = json_decode(file_get_contents(DATABASE_FILE_PATH));
 
-api("manage", function ($action, $callback) {
+api("manage", function ($action, $parameters) {
     global $database;
     if ($action === "session_welcome") {
         $id = random(256);
@@ -13,12 +13,24 @@ api("manage", function ($action, $callback) {
         $database->$id = $word;
         file_put_contents(DATABASE_FILE_PATH, json_encode($database));
         return [true, [$id, $word]];
+    } else if ($action === "session_load") {
+        if (isset($parameters->session)) {
+            if (isset($database->{$parameters->session})) {
+                return [true, $database->{$parameters->session}];
+            } else {
+                return [false, "No such session"];
+            }
+        } else {
+            return [false, "Missing parameters"];
+        }
     } else if ($action === "check") {
         if (isset($parameters->session) && isset($parameters->word)) {
-            if(sha1($parameters->word) === $database->{$parameters->session}){
+            if (sha1($parameters->word) === $database->{$parameters->session}) {
                 unset($database->{$parameters->session});
                 file_put_contents(DATABASE_FILE_PATH, json_encode($database));
                 return [true, "NAT{t00_3asy_4or_me3_2789546347}"];
+            }else{
+                return [false, "Wrong answer"];
             }
         } else {
             return [false, "Missing parameters"];
--- Challenges/EasyShaing/src/scripts/frontend/app.js
+++ Challenges/EasyShaing/src/scripts/frontend/app.js
@@ -1,5 +1,59 @@
+const ENDPOINT = "scripts/backend/manager/manage.php";
+const API = "manage";
+
 function load() {
-    // App Load Code
+    if (!cookie_has("session")) {
+        api(ENDPOINT, API, "session_welcome", {}, (success, result, error) => {
+            if (success) {
+                cookie_push("session", result[0]);
+                get("hash").innerText = "Your thing is: " + result[1];
+            }
+        });
+    } else {
+        api(ENDPOINT, API, "session_load", {session: cookie_pull("session")}, (success, result, error) => {
+            if (success) {
+                get("hash").innerText = "Your thing is: " + result;
+            }
+        });
+    }
 }
 
-// App Code
+function reset() {
+    cookie_push("session", "");
+}
+
+function checkOnBackend() {
+    api(ENDPOINT, API, "session_check", {session: cookie_pull("session"), word: get("in").value}, (success, result, error) => {
+        if (success) {
+            get("output").innerText = result;
+            reset();
+        } else {
+            get("hash").innerText = error;
+        }
+    });
+}
+
+function cookie_pull(name) {
+    name += "=";
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return decodeURIComponent(cookie.substring(name.length, cookie.length));
+        }
+    }
+    return undefined;
+}
+
+function cookie_push(name, value) {
+    const date = new Date();
+    date.setTime(value !== "" ? date.getTime() + (365 * 24 * 60 * 60 * 1000) : 0);
+    document.cookie = name + "=" + encodeURIComponent(value) + ";expires=" + date.toUTCString() + ";domain=" + window.location.hostname + ";path=/";
+}
+
+function cookie_has(name) {
+    return cookie_pull(name) !== undefined;
+}
