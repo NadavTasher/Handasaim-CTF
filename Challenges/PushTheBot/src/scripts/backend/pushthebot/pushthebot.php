8c03d91d03d028b531d9bd73226967a393ebcc6a
Nadav Tasger <tashernadav@gmail.com>
2019-09-21 19:30:06 +0300

    Frontend & Backend update for PTB

--- Challenges/PushTheBot/src/scripts/backend/pushthebot/pushthebot.php
+++ Challenges/PushTheBot/src/scripts/backend/pushthebot/pushthebot.php
@@ -2,18 +2,63 @@
 
 include_once __DIR__ . "/../base/api.php";
 
-const PTB_SPEC = "";
+/**
+ * Specification:
+ * Decoded code should be in the following format:
+ * {
+ *  teamnamehash: TeamNameSHA1
+ *  codes:[
+ *    {
+ *     selectpin: PinNumber
+ *     pinmode: input, output
+ *     command: digitalWrite, analogWrite, servoWrite
+ *     parameter: 0-180
+ *     enable: true,
+ *     length: DelayInMs
+ *    }
+ *  ]
+ * }
+ */
 
 api("pushthebot", function ($action, $parameters) {
     if ($action === "pushcode") {
-        if (isset($parameters->code)) {
-            $code = ptb_sanitize(ptb_parse($parameters->code));
-            return [$code !== null, "Failed to parse or sanitize code"];
+        if (isset($parameters->code) && isset($parameters->team)) {
+            $parsed = ptb_parse($parameters->code);
+            if ($parsed !== null) {
+                if (isset($parsed->teamnamehash)) {
+                    if ($parsed->teamnamehash === sha1($parameters->team)) {
+                        $code = ptb_stringify();
+                    }
+                    return [false, "Verification error"];
+                }
+            }
+            return [false, "Structure error"];
         }
-    } else if ($action === "pullspec") {
-        return [true, PTB_SPEC];
+        return [false, "Parameters error"];
     }
     return [false, "Unknown action"];
 }, true);
 
 echo json_encode($result);
+
+function ptb_parse($encoded)
+{
+    try {
+        return json_decode(ptb_decode($encoded));
+    } catch (Exception $e) {
+        return null;
+    }
+}
+
+function ptb_decode($str)
+{
+    if (strlen($str) === 0) return "";
+    $s = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        $c = ord($str[$i]);
+        if ($c === "3") break;
+        $s <<= 1;
+        if ($c === "2") $s += 1;
+    }
+    return chr($s) . ptb_decode(substr($str, $i + 1));
+}
--- Challenges/PushTheBot/src/scripts/frontend/app.js
+++ Challenges/PushTheBot/src/scripts/frontend/app.js
@@ -2,7 +2,7 @@ const BLINK_CODE = "";
 
 function load() {
     if (cookie_has("team")) get("team").value = cookie_pull("team");
-    if (cookie_has("code")) {
+    if (cookie_has("code") && cookie_pull("code") === "true") {
         show("code-entry");
         hide("blink");
     } else {
@@ -38,11 +38,58 @@ function cookie_has(name) {
 
 function send(compiled) {
     if (get("team").value.length > 0) {
-        api("scripts/backend/pushthebot/pushthebot.php", "pushthebot", "pushcode", {code: compiled, "team": get("team").value}, (success, result, error) => {
-
+        api("scripts/backend/pushthebot/pushthebot.php", "pushthebot", "pushcode", {
+            code: compiled,
+            team: get("team").value
+        }, (success, result, error) => {
+            console.log("---------------------------------------");
+            if (success) {
+                get("output").style.color = "green";
+                get("output").innerText = result;
+            } else {
+                get("output").style.color = "red";
+                get("output").innerText = error;
+            }
         });
     } else {
         get("team").style.position = "relative";
         animate("team", "left", ["0px", "-20px", "0px", "20px", "0px", "-20px", "0px", "20px", "0px"], 0.1);
     }
+}
+
+function compile(rawcode) {
+    return rawcode;
+}
+
+function encode(str) {
+    if (str.length === 0) return "";
+    let c = str[0];
+    let r = "";
+    let cks = 0;
+    c = c.charCodeAt(0);
+    while (c > 0) {
+        if ((c & 1) === 1) {
+            r = "2" + r;
+            cks++;
+        } else {
+            r = "1" + r;
+            cks++;
+            cks++;
+        }
+        c >>= 1;
+    }
+    return r + "3" + encode(str.substr(1));
+}
+
+function decode(str) {
+    if (str.length === 0) return "";
+    let s = 0;
+    let i;
+    for (i = 0; i < str.length; i++) {
+        let c = str[i];
+        if (c === "3") break;
+        s <<= 1;
+        if (c === "2") s += 1;
+    }
+    return String.fromCharCode(s) + decode(str.substr(i + 1));
 }
