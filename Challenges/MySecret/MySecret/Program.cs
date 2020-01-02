2e7f6cff80bfa1e63b16d0ddaf6ce37720ba6d75
Nadav Tasger <tashernadav@gmail.com>
2020-01-03 00:24:21 +0200

    Final?

---
+++ Challenges/MySecret/MySecret/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MySecret
+{
+    internal class Program
+    {
+
+        private static readonly string flag = "H{w0w_w0w_w0w_i_didn7_exp3ct_th4t!}";
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my secret!");
+            Console.Write("Please enter the password: ");
+            Console.ReadLine();
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
